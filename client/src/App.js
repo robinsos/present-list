4ff8c30d507f59ad077ecaad912ef1f9f87f2993
Simon <57892532+robinsos@users.noreply.github.com>
2020-05-10 14:30:09 +0100

    stuff

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+console.log(`Attempting to connect to URI: ${db}`);
+// Connect to mongo
+
+mongoose.connect(db)
+.then(() => console.log('MongoDB Connected...'))
+.catch(err=> console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port: ${port}`));
