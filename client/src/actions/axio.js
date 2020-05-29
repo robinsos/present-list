8b9b6fcf7355def54e0d74e1c2d5310758b3263a
Simon <57892532+robinsos@users.noreply.github.com>
2020-05-29 20:53:20 +0100

    Config change2

--- client/src/actions/axio.js
+++ client/src/actions/axio.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = process.env.MY_BASE_ENV || 'http://localhost:5000/';
+export const baseURL = 'https://damp-tor-32321.herokuapp.com/';
+//export const baseURL = process.env.MY_BASE_ENV || 'http://localhost:5000/';
 
 export const axio = axios.create({ baseURL, timeout: 1000 });
