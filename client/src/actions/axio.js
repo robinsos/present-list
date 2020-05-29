a9d93a1e63f07ce58c74d097a917761efb57b137
Simon <57892532+robinsos@users.noreply.github.com>
2020-05-29 20:49:55 +0100

    Config change2

--- client/src/actions/axio.js
+++ client/src/actions/axio.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export const baseURL = process.env.baseURL || 'http://localhost:5000/';
+export const baseURL = process.env.MY_BASE_ENV || 'http://localhost:5000/';
 
 export const axio = axios.create({ baseURL, timeout: 1000 });
